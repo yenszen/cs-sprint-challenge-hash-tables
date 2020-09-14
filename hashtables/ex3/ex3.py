7b1683c77adb5faf064c0c5c1e0a2ad186c38010
Yen Szen <55312065+yenszen@users.noreply.github.com>
2020-09-14 09:59:21 +0800

    completed ex3, completed MVP

--- hashtables/ex3/ex3.py
+++ hashtables/ex3/ex3.py
@@ -3,6 +3,26 @@ def intersection(arrays):
     YOUR CODE HERE
     """
     # Your code here
+    d = {}
+    result = []
+
+    # the final elements must be in ALL subarrays
+    # hence we only need to assign elements in the first subarray into dict
+    # whichever element in the subsequent subarrays that does not appear in dict will automatically be out of contention
+
+    for el in arrays[0]:
+        if el not in d.keys():
+            d[el] = 1
+
+    for i in range(1, len(arrays)):
+        for el in arrays[i]:
+            if el in d.keys():
+                d[el] += 1
+
+    for k, v in d.items():
+        # if the number has appeared in ALL subararys, it's in the intersection
+        if v == len(arrays):
+            result.append(k)
 
     return result
 
