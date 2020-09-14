453f25a07d63d81ca2bfca8109ad46a1c83d6ee3
Yen Szen <55312065+yenszen@users.noreply.github.com>
2020-09-14 10:10:19 +0800

    completed ex4, completed stretch

--- hashtables/ex4/ex4.py
+++ hashtables/ex4/ex4.py
@@ -3,6 +3,21 @@ def has_negatives(a):
     YOUR CODE HERE
     """
     # Your code here
+    d = {}
+    result = []
+
+    for el in a:
+        # assign all positive integers into dict keys
+        if el > 0 and el not in d.keys():
+            d[el] = None
+    
+    for el in a:
+        if el < 0:
+            # check if the absolute value of the negative integers exists in dict keys
+            check = el * -1
+            # if exists, the positive integer has a corresponding negative integer, hence is added to result
+            if check in d.keys():
+                result.append(check)
 
     return result
 
